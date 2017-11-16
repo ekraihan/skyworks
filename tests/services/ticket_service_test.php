bfc0304c3626f647071eb6bff23ac816ecc27f3e
ekraihan <ekraihan@iupui.edu>
2017-11-15 20:28:00 -0500

    adding get by id

--- tests/services/ticket_service_test.php
+++ tests/services/ticket_service_test.php
@@ -7,14 +7,27 @@
 
 include_once "services/TicketService.php";
 include_once "models/Ticket.php";
+include_once "services/MessageService.php";
+include_once "models/Message.php";
+
+echo "<h3>########## Testing Ticket Service ##########</h3>";
 
 function test_save() {
+    echo "Testing Save <br><br>";
     $ticket = new Ticket();
     $ticket->set_product_id(1)
         ->set_user_id(3);
-    print_r($ticket);
-    TicketService::save($ticket);
+
+    echo "<br><br>";
 }
 
-echo "Testing Save";
+function test_get_by_id() {
+    echo "Test Get By Id <br><br>";
+    print_r(TicketService::get_by_id(46));
+
+    echo "<br><br>";
+}
+
+
 test_save();
+test_get_by_id();
